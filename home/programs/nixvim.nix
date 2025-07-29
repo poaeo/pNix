ad5f5e4c655df8fd8c01e05984d1946e41860229
poaeo <tufanyesil@protonmail.com>
2025-07-29 19:23:02 +0200

    NixVim finally doesn't give error!!

--- home/programs/nixvim.nix
+++ home/programs/nixvim.nix
@@ -7,14 +7,13 @@
     viAlias = true;
     vimAlias = true;
     #Transparent Background 
-    extraLuaConfig = ''
+    extraConfigLua = ''
       -- Transparent background
       vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
       vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
       vim.api.nvim_set_hl(0, "SignColumn", { bg = "none" })
       vim.api.nvim_set_hl(0, "LineNr", { bg = "none" })
       vim.api.nvim_set_hl(0, "EndOfBuffer", { bg = "none" })
-      augroup END
     '';
 
 
