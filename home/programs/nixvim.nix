6b131694660304c5d3b5f775b9314319a4727957
poaeo <tufanyesil@protonmail.com>
2025-07-29 17:34:30 +0200

    FVim

--- home/programs/nixvim.nix
+++ home/programs/nixvim.nix
@@ -6,6 +6,18 @@
     enable = true;
     viAlias = true;
     vimAlias = true;
+    #Transparent Background 
+    extraLuaConfig = ''
+      -- Transparent background
+      vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+      vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
+      vim.api.nvim_set_hl(0, "SignColumn", { bg = "none" })
+      vim.api.nvim_set_hl(0, "LineNr", { bg = "none" })
+      vim.api.nvim_set_hl(0, "EndOfBuffer", { bg = "none" })
+      augroup END
+    '';
+
+
 
     # Basic options
     opts = {
